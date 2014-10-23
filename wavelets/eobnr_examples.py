4516d4efe416ade1d23c204f58870d2ddf06282a
James Clark <james.clark@ligo.org>
2014-10-23 15:53:43 -0400

    fancy

--- wavelets/eobnr_examples.py
+++ wavelets/eobnr_examples.py
@@ -30,6 +30,7 @@ import scipy.io as sio
 
 from matplotlib import pyplot as pl
 import matplotlib.cm as cm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import lal
 import lalsimulation as lalsim
@@ -40,20 +41,16 @@ import lalsimulation as lalsim
 # --- Initial Setup
 # 
 # -----------------------------
-sample_rate = 1024
+sample_rate = 512
 deltaT = 1./sample_rate
 
 # Stellar params
 phiC=0.0
 distance=1e9*lal.PC_SI
-mtot=100
-inclination=0.0
+mtot=500
+inclination=45.0
+mass_ratio=100
 
-mass_ratios=np.arange(1,110,10)
-Nwaveforms=len(mass_ratios)
-
-catalogue_name='eobnr-%.0d-%.0d-%d'%(min(mass_ratios), max(mass_ratios),
-        len(mass_ratios))
 
 # -----------------------------
 #
@@ -61,7 +58,7 @@ catalogue_name='eobnr-%.0d-%.0d-%d'%(min(mass_ratios), max(mass_ratios),
 # 
 # -----------------------------
 
-m2=mtot/(1.+max(mass_ratios))
+m2=mtot/(1.+mass_ratio)
 m1=mtot-m2
 fLow_si = 10.0
 
@@ -76,17 +73,19 @@ time = np.arange(0,hplus.data.length/float(sample_rate),1.0/sample_rate)
 ######################
 # Wavelets !
 
-data = np.copy(hplus.data.data)
+#idx=np.concatenate(np.argwhere(abs(hplus.data.data)>0.01*abs(hplus.data.data)))
+T_max=time[np.argwhere(abs(hplus.data.data)>0.01*abs(hplus.data.data))[-1]]
+idx=range(len(time))
+data = hplus.data.data[idx]
+time = time[idx]
 
 #
 # CWT from pyCWT
 #
-#x = numpy.arange(0,2*numpy.pi,numpy.pi/8.)
-#data = numpy.sin(x**2)
 
 import cwt
 
-scales = 1+np.arange(100)
+scales = 1+np.arange(256)
 mother_wavelet = cwt.SDG(len_signal = len(data), scales = scales,
         normalize = True, fc = 'center')
 wavelet = cwt.cwt(data, mother_wavelet)
@@ -94,75 +93,103 @@ wavelet = cwt.cwt(data, mother_wavelet)
 # --- Plotting
 freqs = 0.5 * sample_rate * wavelet.motherwavelet.fc / wavelet.motherwavelet.scales
 
-pl.figure()
-collevs=np.linspace(min(abs(data)**2), max(abs(data)**2), 1000)
-pl.contourf(time,freqs,np.abs(wavelet.coefs)**2)#, levels=collevs)
+collevs=np.linspace(0, max(map(max,abs(wavelet.coefs)**2)), 100)
+fig, ax_cont = pl.subplots(figsize=(10,5))
+ax_cont.contourf(time,freqs,np.abs(wavelet.coefs)**2, levels=collevs,
+        cmap=cm.gnuplot2)
+ax_cont.set_yscale('log')
+ax_cont.set_xlim(min(time),T_max)
+ax_cont.set_ylim(min(freqs),max(freqs))
+ax_cont.set_xlabel('Time [s]')
+ax_cont.set_ylabel('Frequency [Hz]')
+
+divider = make_axes_locatable(ax_cont)
+
+# time-series
+ax_ts = divider.append_axes("top", 1.2, sharex=ax_cont)
+ax_ts.plot(time, data)
+ax_cont.set_xlim(min(time),T_max)
+ax_ts.set_ylim(-1.1*max(abs(data)), 1.1*max(abs(data)))
+
+# fourier spectrum
+freq_fourier, Pxx = signal.periodogram(data, fs=sample_rate)
+ax_fs = divider.append_axes("right", 1.2, sharey=ax_cont)
+ax_fs.semilogx(Pxx,freq_fourier)
+ax_fs.set_ylim(min(freqs),max(freqs))
+ax_fs.set_xlim(0.01*max(Pxx),1.1*max(Pxx))
+
+pl.setp(ax_ts.get_xticklabels()+ax_fs.get_yticklabels(),visible=False)
+
+pl.tight_layout()
 pl.show()
 
-sys.exit()
 
+
+
+
+
+#   #
+#   # CWT in scipy
+#   #
+#   scales  = np.arange(1,256+1)
+#   wavelet = signal.ricker
+#   cwtcmatr = signal.cwt(data, wavelet, widths)
 #
-# CWT in scipy
 #
-scales  = np.arange(1,256+1)
-wavelet = signal.ricker
-cwtcmatr = signal.cwt(data, wavelet, widths)
-
-
+#   #
+#   # DWT in pyrange(0,2*numpy.pi,numpy.pi/8.)
+#   #data = numpy.sin(x**2)
+#   scales = numpy.arange(10)
 #
-# DWT in pyrange(0,2*numpy.pi,numpy.pi/8.)
-#data = numpy.sin(x**2)
-scales = numpy.arange(10)
-
-mother_wavelet = SDG(len_signal = len(data), scales = np.arange(10), normalize = True, fc ='center')
-
-wavelet = cwt(data, mother_wavelet)
-wavelet.scalogram(origin = 'bottom')
-
-
-
-# --- pywt stuff
-import pywt
-
-wavelet = 'db4'
-level = 4
-order = "freq"  # "normal"
-interpolation = 'nearest'
-cmap = cm.hot
-
-# --- perform wavelet decomposition at levels 1->maxlevel
-#wp = pywt.WaveletPacket(data, wavelet, 'sym', maxlevel=level)
-wd = pywt.wavedec(data, wavelet, 'sym', level=level)
-sys.exit()
-
-# --- Get the nodes of the decomposition at the level you specified
-#nodes = wp.get_level(level, order=order)
-
-# Get the time & frequency resolution at this level
-Fres = sample_rate / (2**level)
-Tres = 1.0/Fres
-time_axis = np.arange(min(time), max(time)+Tres, Tres)
-
-#labels = [n.path for n in nodes]
-scales=len(nodes)
-freqs = pywt.scal2frq(wavelet, (1+np.arange(0,scales))[::-1], 1./sample_rate)
-labels = ['%.2f'%f for f in freqs]
-values = np.array([n.data for n in nodes], 'd')
-values = abs(values)**2
-
-
-# plotting:
-
-f = pl.figure()
-f.subplots_adjust(hspace=0.2, bottom=.03, left=.07, right=.97, top=.92)
-pl.subplot(2, 1, 1)
-pl.title("linchirp signal")
-pl.plot(time, data, 'b')
-
-ax = pl.subplot(2, 1, 2)
-pl.title("Wavelet packet coefficients at level %d" % level)
-pl.imshow(values, interpolation=interpolation, cmap=cmap, aspect="auto",
-    origin="lower", extent=[0, max(time), 0, max(freqs)])
-
-pl.show()
+#   mother_wavelet = SDG(len_signal = len(data), scales = np.arange(10), normalize = True, fc ='center')
+#
+#   wavelet = cwt(data, mother_wavelet)
+#   wavelet.scalogram(origin = 'bottom')
+#
+#
+#
+#   # --- pywt stuff
+#   import pywt
+#
+#   wavelet = 'db4'
+#   level = 4
+#   order = "freq"  # "normal"
+#   interpolation = 'nearest'
+#   cmap = cm.hot
+#
+#   # --- perform wavelet decomposition at levels 1->maxlevel
+#   #wp = pywt.WaveletPacket(data, wavelet, 'sym', maxlevel=level)
+#   wd = pywt.wavedec(data, wavelet, 'sym', level=level)
+#   sys.exit()
+#
+#   # --- Get the nodes of the decomposition at the level you specified
+#   #nodes = wp.get_level(level, order=order)
+#
+#   # Get the time & frequency resolution at this level
+#   Fres = sample_rate / (2**level)
+#   Tres = 1.0/Fres
+#   time_axis = np.arange(min(time), max(time)+Tres, Tres)
+#
+#   #labels = [n.path for n in nodes]
+#   scales=len(nodes)
+#   freqs = pywt.scal2frq(wavelet, (1+np.arange(0,scales))[::-1], 1./sample_rate)
+#   labels = ['%.2f'%f for f in freqs]
+#   values = np.array([n.data for n in nodes], 'd')
+#   values = abs(values)**2
+#
+#
+#   # plotting:
+#
+#   f = pl.figure()
+#   f.subplots_adjust(hspace=0.2, bottom=.03, left=.07, right=.97, top=.92)
+#   pl.subplot(2, 1, 1)
+#   pl.title("linchirp signal")
+#   pl.plot(time, data, 'b')
+#
+#   ax = pl.subplot(2, 1, 2)
+#   pl.title("Wavelet packet coefficients at level %d" % level)
+#   pl.imshow(values, interpolation=interpolation, cmap=cmap, aspect="auto",
+#       origin="lower", extent=[0, max(time), 0, max(freqs)])
+#
+#   pl.show()
 
