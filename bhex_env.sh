798dcf6b7852f9d06ff1eb49c19a9e0db382d210
Alexander Lombardi <allombardi28@gmail.com>
2014-12-09 16:40:33 -0500

    <updating before cloning to solyaris>

---
+++ bhex_env.sh
@@ -0,0 +1,5 @@
+export BHEX_PREFIX=/Users/alex/bhextractor
+export FRGETVECT_PATH=/home/jclark/opt/xpipeline/share/xpipeline/matlab/
+export PATH=/Users/alex/opt/lalsuite/master_install/bin:/Users/alex/opt/lalsuite/master_install/bin:/Users/alex/opt/lalsuite/master_install/bin:/opt/local/bin:/opt/local/sbin:/opt/lscsoft/root/bin:/opt/ldg/bin:/opt/ldg/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/git/bin:/usr/texbin:/Users/alex/ligotools/bin:/Users/alex/bhextractor/bin
+export MATLABPATH=/Users/alex/ligotools/matlab:/Users/alex/bhextractor/bin/matlab
+export PYTHONPATH=/Users/alex/opt/lalsuite/master_install/lib/python2.7/site-packages:/Users/alex/opt/lalsuite/master_install/lib/python2.7/site-packages:/Users/alex/opt/lalsuite/master_install/lib/python2.7/site-packages:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin:/Users/alex/bhextractor/bin/hht
