ddc65e244de5878ff139714bebebe039e1239201
James Clark <james.clark@ligo.org>
2015-05-15 12:43:09 -0400

    updated setup script

--- setup.sh
+++ setup.sh
@@ -5,14 +5,10 @@
 # Get the location of the git repository by finding the full path of this file
 # and stripping off the name
 BHEX_PREFIX=`python -c "import os, sys; print os.path.realpath('${0}')" | sed 's|/setup.sh||g'`
-FRGETVECT_PATH="/home/jclark/opt/xpipeline/share/xpipeline/matlab/"
-MATLABPATH=${MATLABPATH}:"${BHEX_PREFIX}/bin/matlab"
-PYTHONPATH=${PYTHONPATH}:"${BHEX_PREFIX}/bin/hht"
+PYTHONPATH=${PYTHONPATH}:"${BHEX_PREFIX}/bin":"${BHEX_PREFIX}/bin/hht"
 
 echo "export BHEX_PREFIX=${BHEX_PREFIX}" > bhex_env.sh
-echo "export FRGETVECT_PATH=${FRGETVECT_PATH}" >> bhex_env.sh
 echo "export PATH=${PATH}:${BHEX_PREFIX}/bin" >> bhex_env.sh
-echo "export MATLABPATH=${MATLABPATH}" >> bhex_env.sh
 echo "export PYTHONPATH=${PYTHONPATH}" >> bhex_env.sh
 
 
