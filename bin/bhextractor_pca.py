cf1a2170e0f5b7815b9aa0536a8b2754b9cb4e8c
James Clark <james.clark@ligo.org>
2015-06-26 17:37:34 -0400

    separate output for amp/phase PCs

--- bin/bhextractor_pca.py
+++ bin/bhextractor_pca.py
@@ -299,7 +299,8 @@ class waveform_pca:
         fplus  = filename+"_hplusPCs.asc"
         fcross = filename+"_hcrossPCs.asc"
         fcomplex_strain = filename+"_hPCs.dat"
-        fcomplex_ampphase = filename+"_ampPhasePCs.dat"
+        famp = filename+"_AmpPCs.dat"
+        fphase = filename+"_PhasePCs.dat"
 
         # First row contains the mean waveform
         dims = np.shape(self.pca_plus.components_)
@@ -307,7 +308,8 @@ class waveform_pca:
         output_array_cross = np.zeros(shape=(dims[0]+1,dims[1]))
 
         output_array = np.zeros(shape=(dims[0]+1,dims[1]), dtype=complex)
-        output_array_ampphase = np.zeros(shape=(dims[0]+1,dims[1]), dtype=complex)
+        output_array_amp = np.zeros(shape=(dims[0]+1,dims[1]))
+        output_array_phase = np.zeros(shape=(dims[0]+1,dims[1]))
 
         output_array_plus[0,:] = self.pca_plus.mean_
         output_array_plus[1:,:] = self.pca_plus.components_
@@ -317,10 +319,11 @@ class waveform_pca:
         output_array[0,:] = self.pca_plus.mean_ - 1j*self.pca_cross.mean_
         output_array[1:,:] = self.pca_plus.components_ - 1j*self.pca_cross.components_
 
-        output_array_ampphase[0,:] = \
-                self.pca_amp.mean_*np.exp(1j*self.pca_phase.mean_)
-        output_array_ampphase[1:,:] = \
-                self.pca_amp.components_*np.exp(1j*self.pca_phase.components_)
+        output_array_amp[0,:] = self.pca_amp.mean_
+        output_array_amp[1:,:] = self.pca_amp.components_
+
+        output_array_phase[0,:] = self.pca_phase.mean_
+        output_array_phase[1:,:] = self.pca_phase.components_
 
         print "Performing data dump to %s"%fplus
         np.savetxt(fplus, output_array_plus)
@@ -331,9 +334,13 @@ class waveform_pca:
         fpcomplex_strain = open(fcomplex_strain,"wb")
         output_array.tofile(fpcomplex_strain)
 
-        print "Performing data dump to %s"%fcomplex_ampphase
-        fpcomplex_ampphase = open(fcomplex_ampphase,"wb")
-        output_array_ampphase.tofile(fpcomplex_ampphase)
+        print "Performing data dump to %s"%famp
+        fpamp = open(famp,"wb")
+        output_array_amp.tofile(fpamp)
+
+        print "Performing data dump to %s"%fphase
+        fpphase = open(fphase,"wb")
+        output_array_phase.tofile(fpphase)
 
 
     def project_catalogue(self):
